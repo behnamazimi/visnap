6289b55793a30b22cd87ffd107e20466a8f09913
Behnam Azimi <bhnmzm@gmail.com>
2025-10-19 19:52:56 +0200

    Add posthog integration

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,20 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default withMDX(config);
